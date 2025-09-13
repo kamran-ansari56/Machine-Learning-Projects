ad7a833d94700ed91b0163197b89006f2448364e
Korpionn <kamran.ansari56.ka@gmail.com>
2025-09-13 23:58:33 +0530

    mod

--- Olympics Data Analysis/app.py
+++ Olympics Data Analysis/app.py
@@ -1,1 +1,7 @@
-import streamlit
+import streamlit as st
+import pandas as pd
+
+st.sidebar.radio(
+    'Select an option',
+    ('Medal Tally', 'Overall Analysis', 'Country-wise Analysis', 'Athlete-wise Analysis')
+)
